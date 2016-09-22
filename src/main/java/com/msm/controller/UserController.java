9de13d7b0c6ad5da128b992000518703dc4bf722
Venkatesh <venkatesh.sarvapalli@gmail.com>
2016-09-22 12:36:33 +0530

    Menu files. -- Invoice, Product, vendors Managing.

--- src/main/java/com/msm/controller/UserController.java
+++ src/main/java/com/msm/controller/UserController.java
@@ -3,6 +3,7 @@ package com.msm.controller;
 import com.msm.helpers.Layout;
 import com.msm.helpers.UserHelper;
 import com.msm.model.UserAccount;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,8 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class UserController {
 
+
+    @PreAuthorize("hasRole('USER_ROLE')")
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(Model model, HttpServletRequest request) {
         UserAccount userAccount = UserHelper.getCurrentUser();
@@ -27,5 +30,23 @@ public class UserController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model, HttpServletRequest request) {
         return "hello";
+    }
+
+    @PreAuthorize("hasRole('USER_ROLE')")
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
+    public String products(Model model, HttpServletRequest request) {
+        return "product/products";
+    }
+
+    @PreAuthorize("hasRole('USER_ROLE')")
+    @RequestMapping(value = "/invoice", method = RequestMethod.GET)
+    public String invoice(Model model, HttpServletRequest request) {
+        return "invoice/invoice";
+    }
+
+    @PreAuthorize("hasRole('USER_ROLE')")
+    @RequestMapping(value = "/vendors", method = RequestMethod.GET)
+    public String vendors(Model model, HttpServletRequest request) {
+        return "vendor/vendors";
     }
 }
